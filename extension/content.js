0988a80b661fe1d312f5bf47da57f5e4f367973b
Paul Irish <paul.irish@gmail.com>
2015-01-14 09:47:25 -0800

    no more content script

--- extension/content.js
+++
@@ -1,6 +0,0 @@
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request === 'showInputDialog') {
-    var text = window.prompt("CreateLink needs your input");
-    return sendResponse(text);
-  }
-});
