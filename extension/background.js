6a18d79cee1ebd4d9e07c83049d4d2752e090fb4
Paul Irish <paul.irish@gmail.com>
2015-04-10 16:53:12 -0700

    es6 classes. cleanup.

--- extension/background.js
+++ extension/background.js
@@ -1,26 +1,37 @@
 "use strict"
 
-var hiddenInput = document.getElementById('clipboard_object');
-var textToCopy;
-var htmlToCopy;
-var focusHiddenArea = function () {
-    // In order to ensure that the browser will fire clipboard events, we always need to have something selected
-    hiddenInput.value = '';
-    hiddenInput.focus();
-    hiddenInput.select();
-};
-
-
-
 class CreateLink {
     constructor() {
         this.formats = {
             'Plain text': '%text% %url%',
             'HTML': '<a href="%url%">%htmlEscapedText%</a>'
-            // {label: "markdown", format: '[%text%](%url%)' },
-            // {label: "mediaWiki", format: '[%url% %text%]' },
         };
+
+        this.addEventListeners();
+
+        this.hiddenInput = document.getElementById('clipboard_object');
+        this.toClipboard = {};
+
     }
+
+    addEventListeners() {
+        // grab the copy event and hijack it.
+        document.addEventListener('copy', this.handleCopyEvent.bind(this), true);
+
+        // Handle that button click, babe.
+        chrome.browserAction.onClicked.addListener(this.handleBrowserAction);
+    }
+
+    handleBrowserAction(tab) {
+        var opts = {
+            format: 'HTML',
+            text: tab.title,
+            url: tab.url,
+            tab: tab
+        };
+        CL.generateClipboardValues(opts).copyTextToClipboard();
+    }
+
     escapeHTML(text) {
         function convertHTMLChar(c) {
             var charMap = {
@@ -34,64 +45,63 @@ class CreateLink {
         }
         return text ? text.replace(/[&<>'"]/g, convertHTMLChar) : text;
     }
+
     stripTitleSuffixes(title) {
           // these are pretty selfish and represent my own needs more than other peoples'
           return title
           .replace(' - Google Docs', '')
           .replace('- An open-source project to help move the web forward. - Google Project Hosting', '');
-      }
-      generateClipboardValues(opts) {
-
-          // handle the default case
-          htmlToCopy = this.formatLinkText(opts);
-
-          // handle the text fallback.
-          opts.format = 'Plain text';
-          textToCopy = this.formatLinkText(opts);
-          return this;
-      }
-      formatLinkText(opts) {
-        var text = this.stripTitleSuffixes(opts.text).trim();
-        var template = this.formats[opts.format];
-        var data = template
-                    .replace(/%url%/g, opts.url)
-                    .replace(/%text%/g, text)
-                    .replace(/%htmlEscapedText%/g, this.escapeHTML(text))
-                    .replace(/\\t/g, '\t')
-                    .replace(/\\n/g, '\n');
-        return data;
     }
+
+    generateClipboardValues(opts) {
+
+        // handle the default case
+        this.toClipboard.html = this.formatLinkText(opts);
+
+        // handle the text fallback.
+        opts.format = 'Plain text';
+        this.toClipboard.text = this.formatLinkText(opts);
+        return this;
+    }
+
+    formatLinkText(opts) {
+      var text = this.stripTitleSuffixes(opts.text).trim();
+      var template = this.formats[opts.format];
+      var data = template
+                  .replace(/%url%/g, opts.url)
+                  .replace(/%text%/g, text)
+                  .replace(/%htmlEscapedText%/g, this.escapeHTML(text))
+                  .replace(/\\t/g, '\t')
+                  .replace(/\\n/g, '\n');
+      return data;
+    }
+
+    focusHiddenArea() {
+        // In order to ensure that the browser will fire clipboard events, we always need to have something selected
+        this.hiddenInput.value = '';
+        this.hiddenInput.focus();
+        this.hiddenInput.select();
+    };
+
     copyTextToClipboard() {
-        focusHiddenArea();
-        hiddenInput.value = htmlToCopy;
-        hiddenInput.select();
+        this.focusHiddenArea();
+        this.hiddenInput.value = this.toClipboard.html;
+        this.hiddenInput.select();
         document.execCommand('copy');
+    }
+
+    handleCopyEvent (e){
+        e.clipboardData.setData('text/plain', this.toClipboard.text);
+        e.clipboardData.setData('text/html', this.toClipboard.html);
+        this.focusHiddenArea();
+        e.preventDefault();
     }
 }
 
-// For every broswer except IE, we can easily get and set data on the clipboard
-var standardClipboardEvent = function (event) {
-    var clipboardData = event.clipboardData;
-    clipboardData.setData('text/plain', textToCopy);
-    clipboardData.setData('text/html', htmlToCopy);
-};
 
-// Handle that button click, babe.
-chrome.browserAction.onClicked.addListener(function(tab) {
-    var opts = {
-        format: 'HTML',
-        text: tab.title,
-        url: tab.url,
-        tab: tab
-    };
-    new CreateLink().generateClipboardValues(opts).copyTextToClipboard();
-});
 
-// grab the copy event and hijack it.
-window.addEventListener('load', function() {
-    document.addEventListener('copy', function(e){
-        standardClipboardEvent(e);
-        focusHiddenArea();
-        e.preventDefault();
-    }, true);
-}, false);
+var CL = new CreateLink();
+
+
+
+
